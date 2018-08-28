0597ae7d0cf3c383435092d215f704d8f2cd3607
Ratchasit.amo <ratchasit.amo@gmail.com>
2018-08-28 22:51:01 +0700

    [npulser] modified project

--- src/main/java/com/npulser/oo/twitter/app/twitter2slack/controllers/TweetController.java
+++ src/main/java/com/npulser/oo/twitter/app/twitter2slack/controllers/TweetController.java
@@ -1,19 +1,30 @@
 package com.npulser.oo.twitter.app.twitter2slack.controllers;
 
+import com.npulser.oo.twitter.app.twitter2slack.models.ResponseModel;
+import com.npulser.oo.twitter.app.twitter2slack.service.TweetService;
+
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import twitter4j.TwitterException;
+
 @RestController
 public class TweetController {
 
-    @GetMapping("/monitor")
-    public HttpEntity<String> monitoringUser(@RequestParam String username) {
-        
-        
+    private TweetService tweetService;
 
-        return null;
+    public TweetController(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
+
+    @GetMapping("/getTweet")
+    public HttpEntity<ResponseModel> getTweet(@RequestParam String searchQuery) throws TwitterException {
+        tweetService.getTweetMessage(searchQuery);
+        tweetService.getRealTimeTweet(tweetService.getScreenNameId(searchQuery));
+        return new ResponseModel("success", null).build(HttpStatus.OK);
     }
 
 }
