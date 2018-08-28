af8adcc67b6facc050b25f535e8b44a5ab927f51
Ratchasit.amo <ratchasit.amo@gmail.com>
2018-08-28 22:09:48 +0700

    inint commit

---
+++ src/main/java/com/npulser/oo/twitter/app/twitter2slack/Twitter2slackApplication.java
@@ -0,0 +1,12 @@
+package com.npulser.oo.twitter.app.twitter2slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twitter2slackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twitter2slackApplication.class, args);
+	}
+}
---
+++ src/main/java/com/npulser/oo/twitter/app/twitter2slack/controllers/TweetController.java
@@ -0,0 +1,2 @@
+package com.npulser.oo.twitter.app.twitter2slack.controllers;
+
