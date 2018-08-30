c5cfd518b5303ed11d5ae6a703a9eb862b4f0ba0
Ratchasit Amornpornwiwat <ratchasit.amo@ascendcorp.com>
2018-08-30 16:20:11 +0700

    add config for slack channel

--- src/main/java/com/npulser/oo/twitter/app/twitter2slack/service/SlackService.java
+++ src/main/java/com/npulser/oo/twitter/app/twitter2slack/service/SlackService.java
@@ -2,13 +2,18 @@ package com.npulser.oo.twitter.app.twitter2slack.service;
 
 import com.npulser.oo.twitter.app.twitter2slack.models.SlackModel;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SlackService {
-    public void send2Slack(SlackModel slackModel) {
-      RestTemplate restTemplate = new RestTemplate();
-      restTemplate.postForEntity("https://hooks.slack.com/services/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", slackModel, String.class);
-    }
+  
+  @Value("${slack.channel}")
+  private String slackChannel;
+
+  public void send2Slack(SlackModel slackModel) {
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForEntity(slackChannel, slackModel, String.class);
+  }
 }
