14103fef9361b759b21198882cda395197afd9db
Ratchasit.amo <ratchasit.amo@gmail.com>
2018-09-09 23:01:16 +0700

    organize import

--- src/test/java/com/npulser/oo/twitter/app/twitter2slack/services/SlackServiceTest.java
+++ src/test/java/com/npulser/oo/twitter/app/twitter2slack/services/SlackServiceTest.java
@@ -3,12 +3,9 @@ package com.npulser.oo.twitter.app.twitter2slack.services;
 import com.npulser.oo.twitter.app.twitter2slack.models.SlackModel;
 import com.npulser.oo.twitter.app.twitter2slack.service.SlackService;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.internal.verification.Times;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
 
